42efae8aa1a75f8c872c8d0802bafc173e00480d
Shruti Jha <shrutijha1628@gmail.com>
2026-05-04 18:57:01 +0530

    Added live backend API URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mongodb-production-5485.up.railway.app/api' });
+const API = axios.create({ baseURL: 'https://team-task-backend-production-5850.up.railway.app/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
